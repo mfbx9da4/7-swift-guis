e51350a03e2504c18f26f47f4abcc745a1ebb663
David Alberto Adler <dalberto.adler@gmail.com>
2021-11-16 19:02:02 +0000

    Initial Commit

---
+++ asdf/asdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdfApp.swift
+//  asdf
+//
+//  Created by David Adler on 16/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct asdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
